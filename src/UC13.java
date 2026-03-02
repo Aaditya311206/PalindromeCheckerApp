f851acb40e02a97762fb6502ffe02d788fe9f3e0
Aaditya Srivastava <srivastavaaaditya3112@gmail.com>
2026-03-03 01:57:37 +0530

    UC13

---
+++ src/UC13.java
@@ -0,0 +1,33 @@
+public class UC13 {
+    public static void main(String[] args) {
+        String input = "level";
+        PalindromeStrategy strategy = new StackStrategy();
+
+        long startTime = System.nanoTime();
+        boolean result = strategy.check(input);
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
+    }
+}
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
